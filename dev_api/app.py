26ebc4d681abe1758b1de42dd5b9c335018f3e63
Allyson de Oliveira Brandão <Allysonubius>
2020-07-02 21:28:01 -0300

    Realizado o desenvolvimento de uma REST API completa.

---
+++ dev_api/app.py
@@ -0,0 +1,32 @@
+import json
+
+from flask import Flask, jsonify, request,
+
+app = Flask(__name__)
+
+desenvolvedores = [
+    {
+        'nome':'Allyson',
+        'habilidade':['HTML','Flask']
+     },
+    {
+        'nome':'Rafael',
+        'Habildades':['CSS','Bootstrap']
+    }
+]
+
+@app.route('/dev/<int:id>/', methods=['GET','PUT'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        desenvolvedor = desenvolvedores[id]
+        return jsonify(desenvolvedor)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedor[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status':'sucesso','mensagem':'Registro excluido !'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
